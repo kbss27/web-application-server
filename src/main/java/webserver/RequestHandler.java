d87af84f15b91c09d0f34fcf99ae04e18d303daf
hyunwoo <hyunwoo9283@gmail.com>
2018-04-07 09:58:57 +0900

    request_one commit

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +23,29 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            //요구사항 1 - index.html로 응답하기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line;
+            String getURL = "";
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while((line = br.readLine()) != null) {
+                if("".equals(line)) {
+                    break;
+                }
+
+                String[] tmpLine = line.split(" ");
+                if(tmpLine[0].equals(null)) {
+                    getURL = tmpLine[1];
+                }
+
+                stringBuilder.append(line+"\n");
+            }
+
+
+            byte[] body = Files.readAllBytes(new File("./webapp"+getURL).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
---
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,9 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RequestHandlerTest {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+
+}
